f943c8fc404da87eab7e773907aee927d8eabd5f
Christian Schuster <christian.schuster@live.com>
2016-05-05 16:48:41 +0200

    Added spec for MsBuildParsingConfigurationSection.

---
+++ src/SolutionInspector.Api.Tests/Configuration/MsBuildParsing/MsBuildParsingConfigurationSectionSpec.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Configuration;
+using FluentAssertions;
+using Machine.Specifications;
+using SolutionInspector.Api.Configuration.MsBuildParsing;
+using SolutionInspector.Api.Configuration.RuleAssemblyImports;
+using SolutionInspector.TestInfrastructure.Configuration;
+
+#region R# preamble for Machine.Specifications files
+
+// ReSharper disable ArrangeTypeModifiers
+// ReSharper disable ArrangeTypeMemberModifiers
+// ReSharper disable ClassNeverInstantiated.Local
+// ReSharper disable InconsistentNaming
+// ReSharper disable MemberCanBePrivate.Local
+// ReSharper disable NotAccessedField.Local
+// ReSharper disable StaticMemberInGenericType
+// ReSharper disable UnassignedField.Global
+// ReSharper disable UnusedMember.Global
+// ReSharper disable UnusedMember.Local
+// ReSharper disable UnassignedGetOnlyAutoProperty
+
+#endregion
+
+namespace SolutionInspector.Api.Tests.Configuration.MsBuildParsing
+{
+  [Subject(typeof (MsBuildParsingConfigurationSection))]
+  class MsBuildParsingConfigurationSectionSpec
+  {
+    static IMsBuildParsingConfiguration SUT;
+
+    Establish ctx = () =>
+    {
+      SUT = new MsBuildParsingConfigurationSection();
+    };
+
+    class when_deserializing_config
+    {
+      Because of = () => ConfigurationHelper.DeserializeSection((ConfigurationSection) SUT, MsBuildParsingConfigurationSection.ExampleConfiguration);
+
+      It reads_project_build_actions = () =>
+          SUT.ProjectBuildActions.Should().BeEquivalentTo(@"None");
+    }
+  }
+}
