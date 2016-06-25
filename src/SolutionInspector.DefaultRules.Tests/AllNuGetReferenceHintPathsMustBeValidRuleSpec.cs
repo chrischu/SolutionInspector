a1e2e35af7bc6be59cf3f8f38f67e4e0e73e4e16
Christian Schuster <christian.schuster@live.com>
2016-06-25 12:54:41 +0200

    Added AllNuGetReferenceHintPathsMustBeValidRule and corresponding tests.

---
+++ src/SolutionInspector.DefaultRules.Tests/AllNuGetReferenceHintPathsMustBeValidRuleSpec.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using SystemInterface.IO;
+using FakeItEasy;
+using FluentAssertions;
+using Machine.Specifications;
+using SolutionInspector.Api.ObjectModel;
+using SolutionInspector.Api.Rules;
+using SolutionInspector.TestInfrastructure.AssertionExtensions;
+
+#region R# preamble for Machine.Specifications files
+
+// ReSharper disable ArrangeTypeModifiers
+// ReSharper disable ArrangeTypeMemberModifiers
+// ReSharper disable ClassNeverInstantiated.Local
+// ReSharper disable InconsistentNaming
+// ReSharper disable MemberCanBePrivate.Local
+// ReSharper disable NotAccessedField.Local
+// ReSharper disable StaticMemberInGenericType
+// ReSharper disable UnassignedField.Global
+// ReSharper disable UnusedMember.Global
+// ReSharper disable UnusedMember.Local
+// ReSharper disable UnassignedGetOnlyAutoProperty
+
+#endregion
+
+namespace SolutionInspector.DefaultRules.Tests
+{
+  [Subject (typeof(AllNuGetReferenceHintPathsMustBeValidRule))]
+  class AllNuGetReferenceHintPathsMustBeValidRuleSpec
+  {
+    static IFileInfo NuGetReferenceDllFile;
+    static INuGetReference NuGetReference;
+    static IProject Project;
+
+    static AllNuGetReferenceHintPathsMustBeValidRule SUT;
+
+    Establish ctx = () =>
+    {
+      Project = A.Fake<IProject>();
+
+      NuGetReferenceDllFile = A.Fake<IFileInfo>();
+
+      NuGetReference = A.Fake<INuGetReference>();
+      A.CallTo(() => NuGetReference.DllFile).Returns(NuGetReferenceDllFile);
+      A.CallTo(() => NuGetReference.HintPath).Returns("HintPath");
+      A.CallTo(() => NuGetReference.Package.Id).Returns("Id");
+
+      A.CallTo(() => Project.NuGetReferences).Returns(new[] { NuGetReference });
+
+      SUT = new AllNuGetReferenceHintPathsMustBeValidRule();
+    };
+
+    class when_evaluating_and_all_NuGet_reference_hint_paths_are_valid
+    {
+      Establish ctx = () => { A.CallTo(() => NuGetReferenceDllFile.Exists).Returns(true); };
+
+      Because of = () => Result = SUT.Evaluate(Project);
+
+      It does_not_return_violations = () =>
+          Result.Should().BeEmpty();
+
+      static IEnumerable<IRuleViolation> Result;
+    }
+
+    class when_evaluating_and_a_NuGet_references_hint_path_is_invalid
+    {
+      Establish ctx = () => { A.CallTo(() => NuGetReferenceDllFile.Exists).Returns(false); };
+
+      Because of = () => Result = SUT.Evaluate(Project);
+
+      It returns_violation = () =>
+          Result.ShouldAllBeLike(new RuleViolation(SUT, Project, $"The NuGet reference to package 'Id' has an invalid hint path ('HintPath')."));
+
+      static IEnumerable<IRuleViolation> Result;
+    }
+  }
+}
---
+++ src/SolutionInspector.DefaultRules/AllNuGetReferenceHintPathsMustBeValidRule.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using SolutionInspector.Api.ObjectModel;
+using SolutionInspector.Api.Rules;
+
+namespace SolutionInspector.DefaultRules
+{
+  /// <summary>
+  ///   Verifies that all <see cref="INuGetReference" />s in the project have correct hint paths (pointing to an actually
+  ///   existing file).
+  /// </summary>
+  public class AllNuGetReferenceHintPathsMustBeValidRule : ProjectRule
+  {
+    /// <inheritdoc />
+    public override IEnumerable<IRuleViolation> Evaluate (IProject target)
+    {
+      return target.NuGetReferences.Where(r => !r.DllFile.Exists).Select(
+          r => new RuleViolation(
+              this,
+              target,
+              $"The NuGet reference to package '{r.Package.Id}' has an invalid hint path ('{r.HintPath}')."));
+    }
+  }
+}
